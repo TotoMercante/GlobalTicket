9b15bb690dc24e54e7adc3ab6f09fd5a7ec39a87
Tomás Mercante <tomiimercante@gmail.com>
2025-10-10 19:38:33 -0300

    Agrego conexión a API con openapi-generator

---
+++ web/src/api/index.ts
@@ -0,0 +1,16 @@
+export * from "./generated";
+
+import { EventApi, UserApi } from "./generated";
+
+const apis: {
+  event?: EventApi,
+  user?: UserApi
+} = {}
+
+export function getEventApi() {
+  return apis.event || (apis.event = new EventApi())
+}
+
+export function getUserApi() {
+  return apis.user || (apis.user = new UserApi())
+}
