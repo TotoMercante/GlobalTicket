7cd413971955b2cee69d44c522ff4b3d65cb87ac
Tomás Mercante <tomiimercante@gmail.com>
2025-10-10 15:15:10 -0300

    (web): genero proyecto inicial

---
+++ web/src/app/page.tsx
@@ -0,0 +1,11 @@
+export default function Home() {
+  return (
+    <div className="hero is-medium">
+      <div className="hero-body">
+        <h1 className="title">Hello, World!</h1>
+        <p>Created using <code>create-next-app</code></p>
+        <p>Styled using <code>bulma</code></p>
+      </div>
+    </div>
+  );
+}
