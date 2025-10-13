4e88046c7850e5f01c60adbd61d28b3c293bc9b5
Tomás Mercante <tomiimercante@gmail.com>
2025-10-13 16:21:37 -0300

    (api): agrego CORS para app web

--- api/src/main.ts
+++ api/src/main.ts
@@ -3,7 +3,9 @@ import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    cors: { origin: ['http://localhost:3000'] },
+  });
 
   const config = new DocumentBuilder()
     .setTitle('App example')
